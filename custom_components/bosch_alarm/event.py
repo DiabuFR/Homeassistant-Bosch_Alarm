2033943cd18bc237e6f058cb84c08deb8b40a734
Diabu <diabu@diabu.com>
2026-05-05 12:01:02 -0400

    fix type

--- custom_components/bosch_alarm/event.py
+++ custom_components/bosch_alarm/event.py
@@ -1,24 +1,34 @@
-from homeassistant.components.event import EventEntity, EventDeviceClass
+from bosch_alarm_mode2 import Panel
+from homeassistant.components.event import EventEntity
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.entity_platform import AddConfigEntryEntitiesCallback
+
 from homeassistant.helpers.device_registry import DeviceInfo
+from . import BoschAlarmConfigEntry
 from .const import DOMAIN
 
 
-async def async_setup_entry(hass, entry, async_add_entities):
+async def async_setup_entry(
+    hass: HomeAssistant,
+    config_entry: BoschAlarmConfigEntry,
+    async_add_entities: AddConfigEntryEntitiesCallback
+) -> None:
     """Set up Bosch Alarm event entities."""
     # Official integration uses runtime_data to store the panel object
-    panel = entry.runtime_data
-    async_add_entities([BoschHistoryEventEntity(panel, entry)])
+    panel = config_entry.runtime_data
+    async_add_entities([BoschHistoryEventEntity(panel, config_entry)])
 
 
 class BoschHistoryEventEntity(EventEntity):
     """Representation of a Bosch Alarm History Event."""
 
-    _attr_device_class = EventDeviceClass.BUTTON
+    _attr_icon = "mdi:clipboard-text-clock"
+    _attr_device_class = None
     _attr_event_types = ["history_event"]
     _attr_has_entity_name = True
     _attr_translation_key = "history_log"
 
-    def __init__(self, panel, entry):
+    def __init__(self, panel: Panel, entry: BoschAlarmConfigEntry) -> None:
         self._panel = panel
         self._attr_unique_id = f"{entry.entry_id}_history"
         self._attr_device_info = DeviceInfo(
@@ -44,7 +54,7 @@ class BoschHistoryEventEntity(EventEntity):
         last_event = self._panel.events[-1]
 
         # Verify this is a new event based on the timestamp
-        event_time = last_event.datetime.isoformat()
+        event_time = last_event.date.isoformat()
         if event_time == self._last_event_time:
             return
 
